9139791933e60ef9be3f0c16b1b73be8d26573b5
Kyle Baker <87150316+kydogg@users.noreply.github.com>
2024-10-13 00:10:56 -0400

    Initial project setup

---
+++ Culinote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Culinote
+//
+//  Created by Kyle D. Baker on 10/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CulinoteTests/CulinoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  CulinoteTests.swift
+//  CulinoteTests
+//
+//  Created by Kyle D. Baker on 10/13/24.
+//
+
+import Testing
+@testable import Culinote
+
+struct CulinoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
