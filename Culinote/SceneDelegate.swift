8f27ed537cac5888b18d49f0778ef0de5876b691
Kyle Baker <87150316+kydogg@users.noreply.github.com>
2024-10-13 00:55:00 -0400

    Configured app to be handled programmatically. Removed needed references. Set up Core Data stack and Recipe entity.

--- Culinote/SceneDelegate.swift
+++ Culinote/SceneDelegate.swift
@@ -13,10 +13,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        // Ensure the scene is a UIWindowScene
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        // Create a UIWindow and set its windowScene
+        window = UIWindow(windowScene: windowScene)
+
+        // Set the initial view controller
+        let navController = UINavigationController(rootViewController: RecipeListVC())
+
+        // Set the window's rootViewController to the navigation controller
+        window?.rootViewController = navController
+
+        // Make the window visible
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
